f07f333f658dbf52d164f5fb3c48025a32aaa31b
Rifal <rifalsh21@gmail.com>
2025-01-03 11:18:43 +0700

    proggress 3

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard/home');  
+    return view('welcome');  
 });
 
 Route::get('/jobfind', function () {
